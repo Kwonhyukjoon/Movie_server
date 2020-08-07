91b99d7ea4885b04f0f0b9c0d43009c39eb1380c
Kwonhyukjoon <victorehko12@naver.com>
2020-08-07 17:12:21 +0900

    final commit

--- controllers/movie.js
+++ controllers/movie.js
@@ -18,6 +18,25 @@ exports.getMovies = async (req, res, next) => {
   }
 };
 
+// @desc      영화명 검색하는 API
+// @route     GET/api/v1/movies/search?keyword=Y&offset=0&limit=25
+// @request   keyword,offset,limit
+// @response  title
+
+exports.searchMovies = async (req, res, next) => {
+  let offset = req.query.offset;
+  let limit = req.query.limit;
+  let keyword = req.query.keyword;
+  let query = `select * from movie where title like "%${keyword}%" limit ${offset}, ${limit}`;
+
+  try {
+    [rows, fields] = await connection.query(query);
+    res.status(200).json({ success: true, items: rows });
+  } catch (e) {
+    res.status(500).json({ success: false, message: "DB Error", error: e });
+  }
+};
+
 // @desc      연도 내림차순 정렬해서 가져오기
 // @route     GET/api/v1/movies/year?offset=0&limit=25
 // @request   offset,limit
@@ -26,6 +45,22 @@ exports.yearMovies = async (req, res, next) => {
   let offset = req.query.offset;
   let limit = req.query.limit;
   let query = `select * from movie order by year desc limit ${offset}, ${limit}`;
+  try {
+    [rows, fields] = await connection.query(query);
+    res.status(200).json({ success: true, items: rows });
+  } catch (e) {
+    res.status(500).json({ success: false, message: "DB Error", error: e });
+  }
+};
+
+// @desc      관객수 내림차순 정렬해서 가져오기
+// @route     GET/api/v1/movies/attend?offset=0&limit=25
+// @request   offset,limit
+// @response  title, genre, attendance, year
+exports.attendanceMovies = async (req, res, next) => {
+  let offset = req.query.offset;
+  let limit = req.query.limit;
+  let query = `select * from movie order by attendance desc limit ${offset}, ${limit}`;
   try {
     [rows, fields] = await connection.query(query);
     res.status(200).json({ success: true, items: rows });
--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getMovies, yearMovies } = require("../controllers/movie");
+const {
+  getMovies,
+  searchMovies,
+  yearMovies,
+  attendanceMovies,
+} = require("../controllers/movie");
 
 const router = express.Router();
 // 각 경로별로 데이터 가져올 수 있도록, router 셋팅
 router.route("/").get(getMovies);
+router.route("/search").get(searchMovies);
 router.route("/desc").get(yearMovies);
-
+router.route("/pop").get(attendanceMovies);
 module.exports = router;
