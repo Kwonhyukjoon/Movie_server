f7bad3c167a43f0e67a6a37d00eecfa1151887b7
Kwonhyukjoon <victorehko12@naver.com>
2020-08-12 15:11:43 +0900

    last commit

--- routes/favorite.js
+++ routes/favorite.js
@@ -8,10 +8,8 @@ const {
 
 const router = express.Router();
 // 각 경로별로 데이터 가져올 수 있도록, router 셋팅
-router
-  .route("/")
-  .post(auth, addFavorite)
-  .get(auth, getMyFavorites)
-  .delete(auth, deleteFavorite);
+router.route("/").post(auth, addFavorite).get(auth, getMyFavorites);
+
+router.route("/delete").post(auth, deleteFavorite);
 
 module.exports = router;
